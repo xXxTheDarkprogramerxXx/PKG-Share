183b081e5ae644ce3e37c02da57956c408d127f2
xXxTheDarkprogramerxXx <eonvwyk@gmail.com>
2018-05-24 20:10:06 +0200

    Initial commit

---
+++ PKG Share/Form1.Designer.cs
@@ -0,0 +1,255 @@
+﻿namespace PKG_Share
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label2 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.linkLabel4 = new System.Windows.Forms.LinkLabel();
+            this.linkLabel3 = new System.Windows.Forms.LinkLabel();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.linkLabel2 = new System.Windows.Forms.LinkLabel();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(291, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(256, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Welcome To PKG Share download supply and share";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AllowUserToResizeColumns = false;
+            this.dataGridView1.AllowUserToResizeRows = false;
+            this.dataGridView1.BackgroundColor = System.Drawing.Color.Black;
+            this.dataGridView1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.GridColor = System.Drawing.Color.Black;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 36);
+            this.dataGridView1.MultiSelect = false;
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.RowHeadersVisible = false;
+            this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridView1.Size = new System.Drawing.Size(535, 276);
+            this.dataGridView1.TabIndex = 1;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            this.dataGridView1.SelectionChanged += new System.EventHandler(this.dataGridView1_SelectionChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(729, 36);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(43, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "PKG ID";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.InitialImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.InitialImage")));
+            this.pictureBox1.Location = new System.Drawing.Point(567, 36);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(156, 167);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.Black;
+            this.textBox1.ForeColor = System.Drawing.Color.White;
+            this.textBox1.Location = new System.Drawing.Point(729, 52);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(295, 151);
+            this.textBox1.TabIndex = 5;
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(12, 361);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(1012, 23);
+            this.progressBar1.TabIndex = 7;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.linkLabel4);
+            this.groupBox1.Controls.Add(this.linkLabel3);
+            this.groupBox1.ForeColor = System.Drawing.Color.White;
+            this.groupBox1.Location = new System.Drawing.Point(596, 238);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(417, 71);
+            this.groupBox1.TabIndex = 11;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Advanced Shaing";
+            // 
+            // linkLabel4
+            // 
+            this.linkLabel4.AutoSize = true;
+            this.linkLabel4.LinkColor = System.Drawing.Color.White;
+            this.linkLabel4.Location = new System.Drawing.Point(309, 31);
+            this.linkLabel4.Name = "linkLabel4";
+            this.linkLabel4.Size = new System.Drawing.Size(83, 13);
+            this.linkLabel4.TabIndex = 1;
+            this.linkLabel4.TabStop = true;
+            this.linkLabel4.Text = "Mu Suggestions";
+            // 
+            // linkLabel3
+            // 
+            this.linkLabel3.AutoSize = true;
+            this.linkLabel3.LinkColor = System.Drawing.Color.White;
+            this.linkLabel3.Location = new System.Drawing.Point(24, 31);
+            this.linkLabel3.Name = "linkLabel3";
+            this.linkLabel3.Size = new System.Drawing.Size(77, 13);
+            this.linkLabel3.TabIndex = 0;
+            this.linkLabel3.TabStop = true;
+            this.linkLabel3.Text = "Share My PKG";
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.LinkColor = System.Drawing.Color.White;
+            this.linkLabel1.Location = new System.Drawing.Point(741, 220);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(55, 13);
+            this.linkLabel1.TabIndex = 12;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "Download";
+            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
+            // 
+            // linkLabel2
+            // 
+            this.linkLabel2.AutoSize = true;
+            this.linkLabel2.LinkColor = System.Drawing.Color.White;
+            this.linkLabel2.Location = new System.Drawing.Point(905, 220);
+            this.linkLabel2.Name = "linkLabel2";
+            this.linkLabel2.Size = new System.Drawing.Size(89, 13);
+            this.linkLabel2.TabIndex = 13;
+            this.linkLabel2.TabStop = true;
+            this.linkLabel2.Text = "Report a Problem";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(12, 327);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(41, 13);
+            this.label3.TabIndex = 14;
+            this.label3.Text = "Speed:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.ForeColor = System.Drawing.Color.White;
+            this.label4.Location = new System.Drawing.Point(432, 327);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(59, 13);
+            this.label4.TabIndex = 15;
+            this.label4.Text = "Perentage:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.ForeColor = System.Drawing.Color.White;
+            this.label5.Location = new System.Drawing.Point(939, 327);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(85, 13);
+            this.label5.TabIndex = 16;
+            this.label5.Text = "Total Download:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(1036, 396);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.linkLabel2);
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "PKG Share v0.1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.LinkLabel linkLabel4;
+        private System.Windows.Forms.LinkLabel linkLabel3;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+        private System.Windows.Forms.LinkLabel linkLabel2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ PKG Share/Form1.cs
@@ -0,0 +1,266 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Net;
+using System.Threading;
+using System.Diagnostics;
+
+
+namespace PKG_Share
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        string myversion = "1.0.0.0";
+        private DataTable dt = new DataTable();
+        private string filebasename;
+        private string filename;
+        private string filename2;
+        private string Count;
+        WebClient webClient;               // Our WebClient that will be doing the downloading for us
+        Stopwatch sw = new Stopwatch();    // The stopwatch which we will be using to calculate the download spe
+        public void DownloadFile(string urlAddress, string location)
+        {
+            using (webClient = new WebClient())
+            {
+                webClient.DownloadFileCompleted += new AsyncCompletedEventHandler(Completed);
+                webClient.DownloadProgressChanged += new DownloadProgressChangedEventHandler(ProgressChanged);
+
+                // The variable that will be holding the url address (making sure it starts with http://)
+                Uri URL = urlAddress.StartsWith("http://", StringComparison.OrdinalIgnoreCase) ? new Uri(urlAddress) : new Uri("http://" + urlAddress);
+
+                // Start the stopwatch which we will be using to calculate the download speed
+                sw.Start();
+
+                try
+                {
+                    // Start downloading the file
+                    webClient.DownloadFileAsync(URL, location);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+        }
+        // The event that will fire whenever the progress of the WebClient is changed
+        private void ProgressChanged(object sender, DownloadProgressChangedEventArgs e)
+        {
+            // Calculate download speed and output it to labelSpeed.
+            this.label3.Text ="Speed: "+ string.Format("{0} kb/s", (e.BytesReceived / 1024d / sw.Elapsed.TotalSeconds).ToString("0.00"));
+
+            // Update the progressbar percentage only when the value is not the same.
+            progressBar1.Value = e.ProgressPercentage;
+
+            // Show the percentage on our label.
+            this.label4.Text = "Perentage: "+e.ProgressPercentage.ToString() + "%";
+
+            // Update the label with how much data have been downloaded so far and the total size of the file we are currently downloading
+            this.label5.Text ="Total Download: "+ string.Format("{0} MB's / {1} MB's",
+                (e.BytesReceived / 1024d / 1024d).ToString("0.00"),
+                (e.TotalBytesToReceive / 1024d / 1024d).ToString("0.00"));
+        }
+
+        // The event that will trigger when the WebClient is completed
+        private void Completed(object sender, AsyncCompletedEventArgs e)
+        {
+            // Reset the stopwatch.
+            sw.Reset();
+
+            if (e.Cancelled == true)
+            {
+                MessageBox.Show("Download has been canceled.", "Cancled", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                progressBar1.Value = 0;
+                progressBar1.Text = "";
+                label3.Text = "Speed:";
+                label4.Text = "Perentage:";
+                label5.Text = "Total Download:";
+                this.linkLabel1.Text = "Download";
+            }
+            else
+            {
+                progressBar1.Value = 0;
+                progressBar1.Text = "";
+                label3.Text = "Speed:";
+                label4.Text = "Perentage:";
+                label5.Text = "Total Download:";
+                this.linkLabel1.Text = "Download";
+                MessageBox.Show("Download Completed And Added To PKG Folder");
+                this.Text = "PKG Share";
+            }
+        }
+        
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            MessageBox.Show("Hello And Welcome To PKG Share \n\r---Rule Number One Have Fun And Enjoy \n\r---The PKG's supplied have been supplied by users from the PS3 Scene more thanks for them for adding there pkg's", "Hello and Welcome", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+            try
+            {
+                if (System.Net.NetworkInformation.NetworkInterface.GetIsNetworkAvailable() == false & this.Text == "PKG Share v0.1")
+                {
+                    MessageBox.Show("You Need An Active Internet Connection To Run This Application");
+                    Application.Exit();
+                }
+                else
+                {
+                    string appPath = Application.StartupPath;
+                    if (!Directory.Exists(appPath + "\\Workables"))
+                        Directory.CreateDirectory(appPath + "\\Workables");
+
+                    //  DownloadFile(@"http://localhost:1275/xDPx/pkglist.txt", appPath + @"\\db");
+                    //  Thread.Sleep(1000);
+
+                    if (File.Exists(appPath + @"\\db") == true)
+                    {
+                        {
+                            //var PKGid = line.Split(';');
+                            //  for (int i = 0; i <= 6; i++)
+                            // {
+                            //  }
+                            try
+                            {
+                                this.dt.Columns.Add("Game ID");
+                                this.dt.Columns.Add("Name");
+                                this.dt.Columns.Add("Type");
+                                this.dt.Columns.Add("Region");
+                                this.dt.Columns.Add("psnlink");
+                                this.dt.Columns.Add("pnglink");
+                                this.dt.Columns.Add("rapname");
+                                this.dt.Columns.Add("rapdata");
+                                this.dt.Columns.Add("info");
+                                this.dt.Columns.Add("postedby");
+                                string str2;
+                                FileInfo info3 = new FileInfo("db")
+                                {
+                                    Attributes = FileAttributes.Normal
+                                };
+                                byte[] bytes = System.IO.File.ReadAllBytes("db");
+                                StringReader reader = new StringReader(Encoding.GetEncoding(0x4e4).GetString(bytes));
+                                while ((str2 = reader.ReadLine()) != null)
+                                {
+                                    string[] values = str2.Split(new char[] { ';' });
+                                    this.dt.Rows.Add(values);
+                                    this.Count = this.dt.Rows.Count.ToString();
+                                }
+                                this.dataGridView1.DataSource = this.dt;
+                                this.dataGridView1.Columns[0].Width = 100;
+                                this.dataGridView1.Columns[1].Width = 0x160;
+                                this.dataGridView1.Columns[2].Width = 0x48;
+                                this.dataGridView1.Columns[3].Width = 60;
+                                this.dataGridView1.Columns[4].Visible = false;
+                                this.dataGridView1.Columns[5].Visible = false;
+                                this.dataGridView1.Columns[6].Visible = false;
+                                this.dataGridView1.Columns[7].Visible = false;
+                                this.dataGridView1.Columns[8].Visible = false;
+                                this.dataGridView1.Columns[9].Visible = false;
+                                info3.Attributes = FileAttributes.Hidden;
+                                reader.Close();
+                            }
+                            catch
+                            {
+                                MessageBox.Show("Could not load Database", "Database", MessageBoxButtons.OK, MessageBoxIcon.Hand);
+                                FileInfo info4 = new FileInfo("db")
+                                {
+                                    Attributes = FileAttributes.Hidden
+                                };
+                            }
+                            //this.comboBox1.SelectedIndex = this.comboBox1.FindStringExact("All");
+                        }
+
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("An Internal Error Acoured " + ex.Message);
+                System.IO.File.WriteAllText(Application.StartupPath + "\\errorlog.txt", ex.ToString());
+            }
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void dataGridView1_SelectionChanged(object sender, EventArgs e)
+        {
+            if (this.dataGridView1.RowCount != 0)
+            {
+                this.textBox1.Text = this.dataGridView1.CurrentRow.Cells["info"].Value.ToString();
+                //this.size.Text = "Size";
+                this.pictureBox1.ImageLocation = this.dataGridView1.CurrentRow.Cells["pnglink"].Value.ToString();
+                this.pictureBox1.Image = this.pictureBox1.InitialImage;
+               // this.richTextBox2.Text = this.dataGridView1.CurrentRow.Cells["postedby"].Value.ToString();
+                this.label2.Text = this.dataGridView1.CurrentRow.Cells["Game ID"].Value.ToString();
+                //if (this.richTextBox2.Text == "")
+                //{
+                //    this.richTextBox2.Text = "anonymous";
+                //}
+                switch (this.dataGridView1.CurrentRow.Cells["rapdata"].Value.ToString())
+                {
+                    case " ":
+                    case "":
+                        return;
+                }
+            }
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            if (this.dt.Rows.Count != 0)
+            {
+                string extension = Path.GetExtension(this.dataGridView1.CurrentRow.Cells["psnlink"].Value.ToString());
+                int index = extension.IndexOf('?');
+                if (-1 != index)
+                {
+                    extension = extension.Substring(0, index);
+                }
+                string str2 = this.dataGridView1.CurrentRow.Cells["name"].Value.ToString().Replace('\\', ' ').Replace('/', ' ').Replace(':', ' ').Replace('*', ' ').Replace('?', ' ').Replace('"', ' ').Replace('<', ' ').Replace('>', ' ').Replace('|', ' ');
+                this.filebasename = str2 + extension;
+                this.filename = "pkg/" + this.dataGridView1.CurrentRow.Cells["Game ID"].Value.ToString() + " " + this.filebasename;
+                if (this.linkLabel1.Text == "Download")
+                {
+                    if (!System.IO.File.Exists(this.filename) || (MessageBox.Show("Download again?", "Already downloaded!", MessageBoxButtons.OKCancel, MessageBoxIcon.Question, MessageBoxDefaultButton.Button1) == DialogResult.OK))
+                    {
+                        string str4;
+                        if (!Directory.Exists("pkg"))
+                        {
+                            Directory.CreateDirectory("pkg");
+                        }
+                        this.linkLabel1.Text = "Cancel";
+                        this.sw.Start();
+                        string str3 = this.dataGridView1.CurrentRow.Cells["psnlink"].Value.ToString();
+                        if (!str3.StartsWith("http://", StringComparison.OrdinalIgnoreCase) && !str3.StartsWith("https://", StringComparison.OrdinalIgnoreCase))
+                        {
+                            str4 = "http://" + str3;
+                        }
+                        else
+                        {
+                            str4 = str3;
+                        }
+                        DownloadFile(str4, this.filename);
+                       // this.downl.Text = this.dataGridView1.CurrentRow.Cells["name"].Value.ToString();
+                       // this.label16.Text = "Downloading:";
+                    }
+                }
+                else
+                {
+                    this.webClient.CancelAsync();
+                    this.linkLabel1.Text = "Download Package";
+                }
+            }
+        }
+    }
+}
